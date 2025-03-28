4975a1e730ac643d9f4b08256b747a5da31887d2
johnik <kotovjoni@gmail.com>
2025-03-29 00:18:58 +0300

    Init Julia package

---
+++ JuliaLaTeX/src/JuliaLaTeX.jl
@@ -0,0 +1,8 @@
+module JuliaLaTeX
+
+using LaTeXStrings, Unitful, UnitfulLatexify, Latexify, LaTeXDatax
+using Unitful.DefaultSymbols
+
+include("postlatex.jl")
+
+end
---
+++ JuliaLaTeX/test/runtests.jl
@@ -0,0 +1,6 @@
+
+using Test
+
+@testset "Example tests" begin
+    @test true
+end
