50d54eeeae6337b7481b05d9c36415a703c41371
Kandelyabor <200529430+Kandelyabor@users.noreply.github.com>
2025-04-07 07:23:59 +0300

    Fixed displaying units for 10^11

--- JuliaLaTeX/src/JuliaLaTeX.jl
+++ JuliaLaTeX/src/JuliaLaTeX.jl
@@ -1,15 +1,34 @@
+Core.eval(Main,:(wasJuliaLatex=false))
+Main.wasJuliaLatex=@isdefined JuliaLaTeX
 module JuliaLaTeX
 
 using LaTeXStrings, Unitful, UnitfulLatexify, Latexify, LaTeXDatax, DataFrames, PrettyTables
 using Unitful.DefaultSymbols
 
+
 export @Lr_str, @byRow, calcWith, 
     dataToLaTeX, table2datax,
     process_greek, substitute,
-    Constant, Formula, register!, constants2LaTeX, formulas2LaTeX,
+    Constant, Formula, register!,reset_list!, constants2LaTeX, formulas2LaTeX,
     Calculation, calculation2datax
 # export  @Lr_str, @test, process_greek, substitute
 
+import Unitful
+
+if !Main.wasJuliaLatex
+    Core.eval(Main,:(UnitfulLatexify_PrevF=0))
+    Main.UnitfulLatexify_PrevF=last(methods(Latexify.Latexify.apply_recipe,Tuple{Unitful.AbstractQuantity}))
+end
+@latexrecipe function f(
+    q::T; unitformat=:mathrm, siunitxlegacy=false
+) where {T<:Unitful.AbstractQuantity}
+    operation := :*
+    if unitformat === :mathrm || siunitxlegacy
+        return Main.UnitfulLatexify_PrevF(q;:unitformat=>unitformat,:siunitxlegacy=>siunitxlegacy)
+    end
+    env --> :raw
+    return Expr(:latexifymerge, q.val, "\\qty{", "}{", UnitfulLatexify.NakedUnits(unit(q)), "}")
+end
 
 
 function Base.show(io::IO, ::MIME"text/markdown", s::LaTeXString)
