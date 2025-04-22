6fd5858dec227fcd6febf605a1f581c6670ad949
Kandelyabor <200529430+Kandelyabor@users.noreply.github.com>
2025-04-22 10:17:22 +0300

    Better switch

--- JuliaLaTeX/src/utils/switch.jl
+++ JuliaLaTeX/src/utils/switch.jl
@@ -1,36 +1,108 @@
+module SwitchMacroUtils
+export @switch
 
+try_replace(expression::Symbol, replaced::Base.RefValue{Bool}, compare_name::Symbol) =
+    expression == :_ ? (replaced.x = true; compare_name) : expression
+try_replace(expression::Number, replaced::Base.RefValue{Bool}, compare_name::Symbol) =
+    expression
+try_replace(expression::Expr, replaced::Base.RefValue{Bool}, compare_name::Symbol) =
+    Expr(expression.head, [try_replace(x, replaced, compare_name) for x in expression.args]...)
+
+cached_expr_symbol::Symbol = :___value___
+
+
+"
+Example 1:
+```
+@switch typeof(reference) => {
+    _ <: Vector => println(Vector);
+    _ <: Tuple => println(Tuple);
+    _ => println(Missing);
+}
+```
+Transformed to
+```
+local ___value___ = typeof(reference)
+if ___value___ <: Vector
+    println(Vector)
+elseif ___value___ <: Tuple
+    println(Tuple)
+else
+    println(Missing)
+end
+```
+
+Example 2:
+```
+@switch object => {
+    typeof(_) <: Vector => println(Vector);
+    3 => println(\"Number: \",'3');
+    _ => println(\"Other\");
+}
+```
+Transformed to
+```
+local ___value___ = object
+if typeof(___value___) <: Vector
+    println(Vector)
+elseif ___value___ == 3
+    println(\"Number: \", '3')
+else
+    println(\"Other\")
+end
+```
+
+"
 macro switch(expr)
     @assert expr.head == :call
     @assert expr.args[1] == :(=>)
     value = expr.args[2]
 
 
-    body = expr.args[3]
+    body::Expr = expr.args[3]::Expr
 
-    prevSum = quote
-        local ___value___ = $value
+    prevSum::Expr = quote
+        local $cached_expr_symbol = $value
     end
-    startBody = prevSum
-    ifSym = :if
+    startBody::Expr = prevSum
+    ifSym::Symbol = :if
     # println(string(expr))
+    boolRef::Base.RefValue = Ref(false)
     for arg in body.args
-        @assert typeof(arg)==Expr
-        arg.head==:parameters && continue
+        @assert typeof(arg) == Expr
+        arg.head == :parameters && continue
         @assert arg.args[1] == :(=>) string(arg)
+        match_expression = arg.args[2]
 
-        ifStmt = if arg.args[2] == :_
+        ifStmt = if match_expression == :_
             Expr(:block, arg.args[3])
         else
-            Expr(ifSym, Expr(:call, :(==), :___value___, arg.args[2]), arg.args[3])
+            boolRef.x = false
+            match_expression = try_replace(match_expression, boolRef, cached_expr_symbol)
+            if boolRef.x
+                Expr(ifSym, match_expression, arg.args[3])
+            else
+                Expr(ifSym, Expr(:call, :(==), cached_expr_symbol, match_expression), arg.args[3])
+            end
         end
 
         push!(prevSum.args, ifStmt)
         prevSum = ifStmt
         ifSym = :elseif
     end
-    
+
     # Expr(:block,startBody)|>Base.remove_linenums! |> string|>println
-    return esc(Expr(:block,startBody))
+    return esc(Expr(:block, startBody))
 
 end
 
+macro using_self(expr)
+    esc(Expr(:block,
+        Expr(:(=), Symbol("@switch"), var"@switch"),    
+        :()
+        ))
+end
+
+end
+
+SwitchMacroUtils.@using_self(using .SwitchMacroUtils)
