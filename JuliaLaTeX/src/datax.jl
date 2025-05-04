38c5db6e02942960c67ce54fe71ce60ec6c03dc9
Kandelyabor <200529430+Kandelyabor@users.noreply.github.com>
2025-05-04 18:43:31 +0300

    Better @Lr_str

--- JuliaLaTeX/src/datax.jl
+++ JuliaLaTeX/src/datax.jl
@@ -1,8 +1,2 @@
-import LaTeXDatax
 
-store_datax(io::IO, (k, v)::Pair{String,Any}) = LaTeXDatax.printkeyval(io, k, v)
-function store_datax(io::IO, d::Vector{Pair{String,Any}})
-    for (k, v) in d
-        LaTeXDatax.printkeyval(io, k, v)
-    end
-end
+
--- JuliaLaTeX/src/latex.jl
+++ JuliaLaTeX/src/latex.jl
@@ -1,98 +1,33 @@
-"""
-Analog @L_str but without \$
+@doc raw"""
+    L"..."
 
-No character escaping: `Lr"2 \\cdot 2"` is valid
+Creates a `LaTeXString` and is equivalent to `raw_latexstring(raw"...")`, except that
+`%$` can be used for interpolation.
+
+```jldoctest
+julia> Lr"x = \sqrt{2}"
+L"x = \sqrt{2}"
+
+julia> Lr"x = %$(sqrt(2))"
+L"x = 1.4142135623730951"
+```
 """
 macro Lr_str(str::String)
-    LaTeXString(str)
+    expr = LaTeXStrings.var"@L_str"(__source__, __module__, str)
+    expr.args[1].args[1] = GlobalRef(@__MODULE__, :raw_latexstring)
+    expr
+end
+@doc raw"""
+    raw_latexstring(args...)
+    
+Creates a `LaTeXString` object directly from the input arguments
+
+"""
+function raw_latexstring(s...)
+    return raw_latexstring(string(s...))
 end
 
-#= 
-function dataToLaTeX(data::DataFrame)
-    io = IOContext(IOBuffer())
-    dataToLaTeX(io, data)
-    return String(take!(io.io))
-end =#
-
-function dataToLaTeX(filename::String, data::DataFrame, @nospecialize(nameAliases::Pair...); permissions::String="w")
-    mkpath(dirname(filename))
-    open(filename, permissions) do io
-        dataToLaTeX(io, data, nameAliases...)
-    end
+function raw_latexstring(s::String)
+    return LaTeXString(s)
 end
 
-function dataToLaTeX(io::IO, data::DataFrame, @nospecialize(nameAliases::Pair...))
-    set_default(unitformat=:siunitx, fmt=FancyNumberFormatter(4))
-    nameAliases = Dict(nameAliases...)
-    # @show
-    local ret = pretty_table(io,
-        [LatexCell(latexify(
-            if typeof(v) == Evaluatable
-                UnitSystem.extractValue(
-                    UnitSystem.applyUnitTo(Core.eval(eval_module(),v.inlineWithUnits), v.unit)
-                )
-            else
-                UnitSystem.extractValue(v)
-            end
-        )) for v in Tables.matrix(data)]
-        ; backend=Val(:latex), alignment=:c, vlines=:all,
-        header=[
-            string(raw"$",
-                latexify(get(nameAliases, name, name); env=:raw),
-                ",\\;",
-                if typeof(u) == Evaluatable
-                    # UnitSystem.applyUnitTo(u.inlineWithUnits |> eval,u.unit)
-                    latexify(u.unit)
-                else
-                    latexify(UnitSystem.extractUnit(u))
-                end,
-                raw"$")
-            for (name, u) in zip(names(data), data[1, :])
-        ] .|> LatexCell
-    )
-    reset_default()
-    return ret
-end
-
-
-function table2datax(data::DataFrame, name::String)
-    io = IOContext(IOBuffer())
-    table2datax(io, data, name)
-    return String(take!(io.io))
-end
-function table2datax(filename::String, data::DataFrame, name::String, permissions::String="w")
-    mkpath(dirname(filename))
-    open(filename, permissions) do io
-        table2datax(io, data, name, permissions)
-    end
-end
-function table2datax(io::IO, data::DataFrame, name::String, permissions::String="w")
-    set_default(unitformat=:siunitx, fmt=FancyNumberFormatter(4), env=:raw)
-
-    for r in 1:nrow(data)
-        for c in 1:ncol(data)
-            key = "$name[$(names(data, c)[1]),$r]"
-            LaTeXDatax.printkeyval(io, key, toBaseUnit(data[r, c]))
-        end
-    end
-
-    for c in 1:ncol(data)
-        col = data[!, c]
-        typeof(col) != Vector{Evaluatable} && continue
-        form = names(data, c)[1]
-        for r in 1:nrow(data)
-            LaTeXDatax.printkeyval(io,
-                "calc/$form[$r]",
-                latexify(data[r, c].displayCalculated; env=:raw)
-            )
-
-            f::Formula = Core.eval(eval_module(), Symbol(form))
-            d = latexifyDisplayName(f.displayName)
-            LaTeXDatax.printkeyval(io,
-                "expr/$form[$r]",
-                latexify(Expr(:(=), LaTeXString(d), data[r, c].displayCalculated); env=:raw)
-            )
-        end
-    end
-    reset_default()
-end
