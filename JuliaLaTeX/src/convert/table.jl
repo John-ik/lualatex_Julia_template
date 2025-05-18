287b0c915b0dc1e5d1c6cd5b30a15513cbb34847
Kandelyabor <200529430+Kandelyabor@users.noreply.github.com>
2025-05-18 21:02:41 +0300

    Fixed nothing in calculation columns

--- JuliaLaTeX/src/convert/table.jl
+++ JuliaLaTeX/src/convert/table.jl
@@ -7,31 +7,31 @@ function data_to_LaTeX_table(data::DataFrame)
     return String(take!(io.io))
 end =#
 
-function data_to_LaTeX_table(filename::String, data::Union{DataFrame,Calculation}, @nospecialize(nameAliases::Pair...); permissions::String="w")
+function data_to_LaTeX_table(filename::String, data::Union{DataFrame, Calculation}, @nospecialize(nameAliases::Pair...); permissions::String = "w")
     mkpath(dirname(filename))
     open(filename, permissions) do io
-        data_to_LaTeX_table(io, data, nameAliases...)
+        return data_to_LaTeX_table(io, data, nameAliases...)
     end
 end
 
 function data_to_LaTeX_table(io::IO, data::DataFrame, @nospecialize(nameAliases::Pair...))
-    set_default(unitformat=:siunitx, fmt=FancyNumberFormatter(4))
+    set_default(unitformat = :siunitx, fmt = FancyNumberFormatter(4))
     nameAliases = Dict(nameAliases...)
     # @show
     local ret = pretty_table(io,
         [LatexCell(latexify(
             if typeof(v) == Evaluatable
                 UnitSystem.extract_value(
-                    UnitSystem.applyUnitTo(Core.eval(eval_module(), v.inlineWithUnits), v.unit)
+                    UnitSystem.applyUnitTo(Core.eval(eval_module(), v.inlineWithUnits), v.unit),
                 )
             else
                 UnitSystem.extract_value(v)
-            end
+            end,
         )) for v in Tables.matrix(data)]
-        ; backend=Val(:latex), alignment=:c, vlines=:all,
-        header=[
+        ; backend = Val(:latex), alignment = :c, vlines = :all,
+        header = [
             string(raw"$",
-                latexify(get(nameAliases, name, name); env=:raw),
+                latexify(get(nameAliases, name, name); env = :raw),
                 ",\\;",
                 if typeof(u) == Evaluatable
                     # UnitSystem.applyUnitTo(u.inlineWithUnits |> eval,u.unit)
@@ -41,14 +41,16 @@ function data_to_LaTeX_table(io::IO, data::DataFrame, @nospecialize(nameAliases:
                 end,
                 raw"$")
             for (name, u) in zip(names(data), data[1, :])
-        ] .|> LatexCell
+        ] .|> LatexCell,
     )
     reset_default()
     return ret
 end
 
+
+
 function data_to_LaTeX_table(io::IO, data::Calculation, @nospecialize(nameAliases::Pair...))
-    set_default(unitformat=:siunitx, fmt=FancyNumberFormatter(4))
+    set_default(unitformat = :siunitx, fmt = FancyNumberFormatter(4))
     nameAliases = Dict(nameAliases...)
     # @show
     local ret = pretty_table(io,
@@ -56,10 +58,10 @@ function data_to_LaTeX_table(io::IO, data::Calculation, @nospecialize(nameAliase
             LatexCell ∘ latexify ∘ UnitSystem.extract_value ∘ eval_with_units,
             isa(v_list, Vector) ? v_list : [v_list])
               for (_, v_list) in data)...)
-        ; backend=Val(:latex), alignment=:c, vlines=:all,
-        header=[
+        ; backend = Val(:latex), alignment = :c, vlines = :all,
+        header = [
             string(raw"$",
-                latexify(get(nameAliases, string(name), name); env=:raw),
+                latexify(get(nameAliases, string(name), name); env = :raw),
                 ",\\;",
                 begin
                     list_type = typeof(u_list)
@@ -68,6 +70,29 @@ function data_to_LaTeX_table(io::IO, data::Calculation, @nospecialize(nameAliase
                         u_list[1], eltype(u_list)
                     else
                         u_list, list_type
+                    end
+                    has_nothing = false
+                    if list_type <: AbstractArray
+                        @label try_again
+                        if isa(elem_type, Union)
+                            if elem_type.a === Nothing
+                                has_nothing = true
+                                elem_type = elem_type.b
+                                @goto try_again
+                            elseif elem_type.b === Nothing
+                                has_nothing = true
+                                elem_type = elem_type.a
+                                @goto try_again
+                            else
+                            end
+                        end
+                        if has_nothing
+                            i = 1
+                            while first_elem === nothing
+                                first_elem = u_list[i]
+                                i += 1
+                            end
+                        end
                     end
                     # @show elem_type
                     # if elem_type==Any && list_type<:AbstractArray
@@ -85,7 +110,7 @@ function data_to_LaTeX_table(io::IO, data::Calculation, @nospecialize(nameAliase
                     end
                 end,
                 raw"$") |> LatexCell
-            for (name, u_list) in data]
+            for (name, u_list) in data],
     )
     reset_default()
     return ret
