2bf7a5aaf4963369a68cf3f683f701840f3542a9
Kandelyabor <200529430+Kandelyabor@users.noreply.github.com>
2025-05-18 21:09:27 +0300

    Now ExponentNumber acts like a Number in UnitSystem.SI.preferredUnit & toPreferred

--- JuliaLaTeX/src/rounding/exponent/unitsystem_support.jl
+++ JuliaLaTeX/src/rounding/exponent/unitsystem_support.jl
@@ -1,2 +1,7 @@
 UnitSystem.extract_unit(::ExponentNumber) = nothing
 UnitSystem.extract_value(x::ExponentNumber) = x
+
+UnitSystem.SI.preferredUnit(x::ExponentNumber) = nothing
+UnitSystem.SI.toPreferred(x::ExponentNumber) = x
+# UnitSystem.SI.convertExpr(x::Unitful.Quantity{ExponentNumber}) = UnitSystem.SI.convertExpr()(x)|>eval
+
